51a45d20bb748b14fcaa22d93a82835b35db66a9
Ching367436 <44335786+Ching367436@users.noreply.github.com>
2020-06-26 13:13:44 +0800

    add instruction

---
+++ instruction/script.js
@@ -0,0 +1,13 @@
+let img = document.querySelector('img'),
+    current = 0;
+const imgs = ['./alpha_beta_pruning/1.png', './alpha_beta_pruning/2.png', './alpha_beta_pruning/3.png', './alpha_beta_pruning/4.png', './alpha_beta_pruning/5.png', './alpha_beta_pruning/6.png', './alpha_beta_pruning/7.png', './alpha_beta_pruning/8.png', './alpha_beta_pruning/9.png'];
+
+img.addEventListener('click', function (e) {
+    if (e.offsetX >= this.offsetWidth / 2) {
+        if (++current >= imgs.length) { current = 0; }
+        img.src = imgs[current];
+    } else {
+        if (--current < 0) { current = imgs.length - 1; }
+        img.src = imgs[current];
+    }
+});
